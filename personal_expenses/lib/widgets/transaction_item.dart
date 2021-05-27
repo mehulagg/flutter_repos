a8b3327d528b86598f50a84a9663693eead43425
Mehul Aggarwal <jonygupta80@gmail.com>
2021-05-27 18:29:22 +0000

    may 27

--- personal_expenses/lib/widgets/transaction_item.dart
+++ personal_expenses/lib/widgets/transaction_item.dart
@@ -1,10 +1,12 @@
+import 'dart:math';
+
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:personal_expenses/models/transaction.dart';
 
-class TransactionItem extends StatelessWidget {
+class TransactionItem extends StatefulWidget {
   final Transaction transaction;
   final Function deleteTx;
 
@@ -13,6 +15,26 @@ class TransactionItem extends StatelessWidget {
     @required this.transaction,
     @required this.deleteTx,
   }) : super(key: key);
+
+  @override
+  _TransactionItemState createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+  Color _bgColor;
+
+  @override
+  void initState() {
+    const availableColors = [
+      Colors.red,
+      Colors.purple,
+      Colors.amber,
+      Colors.teal,
+    ];
+
+    _bgColor = availableColors[Random().nextInt(4)];
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,20 +51,21 @@ class TransactionItem extends StatelessWidget {
                 color: Theme.of(context).primaryColorDark,
               ),
             ),
+            color: _bgColor,
           ),
           child: Padding(
             padding: const EdgeInsets.all(6.0),
             child: FittedBox(
-              child: Text("\$${transaction.amount.toStringAsFixed(2)}"),
+              child: Text("\$${widget.transaction.amount.toStringAsFixed(2)}"),
             ),
           ),
         ),
         title: Text(
-          transaction.title,
+          widget.transaction.title,
           style: Theme.of(context).textTheme.headline6,
         ),
         subtitle: Text(
-          DateFormat.yMMMd().format(transaction.date),
+          DateFormat.yMMMd().format(widget.transaction.date),
         ),
         trailing: MediaQuery.of(context).size.width > 460
             ? TextButton.icon(
@@ -51,12 +74,12 @@ class TransactionItem extends StatelessWidget {
                 ),
                 icon: const Icon(CupertinoIcons.delete),
                 label: const Text('Delete'),
-                onPressed: () => deleteTx(transaction.id),
+                onPressed: () => widget.deleteTx(widget.transaction.id),
               )
             : IconButton(
                 icon: const Icon(FluentIcons.delete_24_filled),
                 color: Theme.of(context).errorColor,
-                onPressed: () => deleteTx(transaction.id),
+                onPressed: () => widget.deleteTx(widget.transaction.id),
               ),
       ),
     );
