cf9a1916c855cd93de2e307ee0fcda2e2bfd51b3
Mehul Aggarwal <53606356+mehulagg@users.noreply.github.com>
2021-07-30 18:21:08 +0000

    july 30

--- shop/lib/main.dart
+++ shop/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/providers/products.dart';
 import 'package:shop/screens/products_overview_screen.dart';
 
 void main() {
@@ -8,14 +10,17 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.deepOrange,
-          fontFamily: 'Lato'),
-      title: 'MyShop',
-      home: ProductsOverviewScreen(),
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            fontFamily: 'Lato'),
+        title: 'MyShop',
+        home: ProductsOverviewScreen(),
+      ),
     );
   }
 }
